c63fd4d5261f1dd5837bfd9e82a2a9a428717acd
chamaine <chamaine@wellington-studio.com>
2017-11-09 21:26:28 -0800

    homework

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Constructor-Hangman
+
+homework update
---
+++ hangman.js
@@ -0,0 +1,6 @@
+// constructor function which can take in a series of values and create objects
+// with the properties contained inside
+function Words(location, phrase, letter) {
+  this.location = location;
+  this.phrase = phrase;
+  this.letter = letter;
